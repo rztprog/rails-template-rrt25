e7652de965dddb88171e66f91051e8b5929da8de
Rztprog <45171753+rztprog@users.noreply.github.com>
2025-02-13 20:36:43 +0100

    Update folder name

--- minimal.rb
+++ minimal.rb
@@ -37,8 +37,8 @@ end
 run "rm -rf app/assets/stylesheets"
 run "rm -rf vendor"
 run "curl -L https://github.com/rztprog/rails-template-rrt25/raw/refs/heads/main/rails-rrt25-stylesheets-master.zip > stylesheets.zip"
-run "unzip stylesheets.zip -d app/assets && rm -f stylesheets.zip && rm -f app/assets/rails-stylesheets-master/README.md"
-run "mv app/assets/rails-stylesheets-master app/assets/stylesheets"
+run "unzip stylesheets.zip -d app/assets && rm -f stylesheets.zip && rm -f app/assets/rails-rrt25-stylesheets-master/README.md"
+run "mv app/assets/rails-rrt25-stylesheets-master app/assets/stylesheets"
 
 # Layout
 ########################################
