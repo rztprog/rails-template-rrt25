f6a03c904d54958ee5d637b3375f983af1b6dbf4
Rztprog <45171753+rztprog@users.noreply.github.com>
2026-03-21 18:15:44 +0100

    Update minimal.rb
    add \n

--- minimal.rb
+++ minimal.rb
@@ -23,7 +23,7 @@ inject_into_file "Gemfile", before: "group :development, :test do" do
   RUBY
 end
 
-inject_into_file "Gemfile", after: "group :development, :test do" do
+inject_into_file "Gemfile", after: "group :development, :test do\n" do
   <<~RUBY
     gem "dotenv-rails"
     gem "faker"
